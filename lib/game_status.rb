4ec3cb85eff6f47a19b6f2168278157896e98650
Karson Kalt <kars.kalt@gmail.com>
2020-08-10 20:56:44 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,7 +17,8 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-    if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && "board"[win_combination[2]] == "X"
+    if
+      (board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && "board"[win_combination[2]] == "X") || (board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && "board"[win_combination[2]] == "O") 
     return
       win_combination
     else
